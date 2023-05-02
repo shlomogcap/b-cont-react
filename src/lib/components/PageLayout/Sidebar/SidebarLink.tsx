e2f8120097b7029425a73089ef63a7e3f9f09ad3
shlomo glikman <shlomog@capitolis.com>
2023-05-02 15:14:50 +0300

    add useBreakpoint and show sidebarLink only for BigDesktop BP

--- src/lib/components/PageLayout/Sidebar/SidebarLink.tsx
+++ src/lib/components/PageLayout/Sidebar/SidebarLink.tsx
@@ -2,16 +2,18 @@ import { useRouter } from 'next/router';
 import { StyledSidebarLink } from './Sidebar.styled';
 import { SidebarLinkProps } from './Sidebar.types';
 import { Tooltip } from '../../commons/Tooltip/Tooltip';
+import { useBreakpoint } from '@/lib/hooks/useBreakpoint';
+import { Breakpoints } from '../../consts/stylesConsts';
 
 export const SidebarLink = ({ text, href, icon }: SidebarLinkProps) => {
   const { asPath } = useRouter();
   const isActive = asPath === href;
-  return (
-    <Tooltip content={text}>
-      <StyledSidebarLink href={href} className={`${isActive ? 'active' : ''}`}>
-        {icon}
-        <div className='text'>{text}</div>
-      </StyledSidebarLink>
-    </Tooltip>
+  const isBigDesktop = useBreakpoint(`(min-width: ${Breakpoints.BigDesktop})`);
+  const link = (
+    <StyledSidebarLink href={href} className={`${isActive ? 'active' : ''}`}>
+      {icon}
+      <div className='text'>{text}</div>
+    </StyledSidebarLink>
   );
+  return isBigDesktop ? link : <Tooltip content={text}>{link}</Tooltip>;
 };
