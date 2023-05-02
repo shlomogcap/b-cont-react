11bb918c2c9dada5410b20d01c921d24ca5899f4
shlomo glikman <shlomog@capitolis.com>
2023-05-02 17:51:58 +0300

    create DISPLAY_TEXTS object

---
+++ src/lib/components/consts/displayTexts.ts
@@ -0,0 +1,36 @@
+import { ProjectType } from './projectTypes';
+import { Routes } from './routes';
+
+type Lang = 'he' | 'en';
+
+type DisplayTextMapping = {
+  projectType: Record<ProjectType, string>;
+  routeNames: Record<Routes, string>;
+};
+
+export const DISPLAY_TEXTS: Record<Lang, DisplayTextMapping> = {
+  he: {
+    projectType: {
+      [ProjectType.Residential]: 'מגורים',
+      [ProjectType.PublicSpace]: 'ציבורי',
+      [ProjectType.Entrepreneurship]: 'יזמות',
+    },
+    routeNames: {
+      [Routes.Projects]: 'פרוייקטים',
+      [Routes.Vendors]: 'קבלנים',
+      [Routes.Settings]: 'הגדרות',
+    },
+  },
+  en: {
+    projectType: {
+      [ProjectType.Residential]: 'Residential',
+      [ProjectType.PublicSpace]: 'Public Space',
+      [ProjectType.Entrepreneurship]: 'Entrepreneurship',
+    },
+    routeNames: {
+      [Routes.Projects]: 'Projects',
+      [Routes.Vendors]: 'Vendors',
+      [Routes.Settings]: 'Settings',
+    },
+  },
+};
