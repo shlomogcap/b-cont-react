fec22a7bcc99ac30fdc1e5949f0cc9113d139ada
shlomo glikman <shlomog@capitolis.com>
2023-05-11 09:01:35 +0300

    refactor query params names

--- src/lib/consts/routes.ts
+++ src/lib/consts/routes.ts
@@ -1,9 +1,11 @@
+export const PROJECT_TYPE_QUERY = 'projectType';
+export const PROJECT_ID_QUERY = 'projectId';
+export const CONTRACT_ID_QUERY = 'contractId';
+
 export enum Routes {
   Projects = '/app/projects',
   Vendors = '/app/vendors',
   Settings = '/app/settings',
-  Project = '/app/projects/[projectId]',
-  Contract = '/app/projects/[projectId]/contracts/[contractId]',
+  Project = `/app/projects/[${PROJECT_ID_QUERY}]`,
+  Contract = `/app/projects/[${PROJECT_ID_QUERY}]/contracts/[${CONTRACT_ID_QUERY}]`,
 }
-
-export const PROJECT_TYPE_QUERY = 'projectType';
--- src/pages/app/projects/[projectId]/index.tsx
+++ src/pages/app/projects/[projectId]/index.tsx
@@ -1,9 +1,12 @@
 import { ProjectPage } from '@/lib/components/ProjectPage/ProjectPage';
+import { PROJECT_ID_QUERY } from '@/lib/consts/routes';
 import { queryParamToString } from '@/lib/utils/queryParamToString';
 import { useRouter } from 'next/router';
 import React from 'react';
 
 export default function ProjectRoute() {
   const { query } = useRouter();
-  return <ProjectPage projectId={queryParamToString(query, 'projectId')} />;
+  return (
+    <ProjectPage projectId={queryParamToString(query, PROJECT_ID_QUERY)} />
+  );
 }
