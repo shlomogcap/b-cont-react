95ecdbc9d7e2b6c3d7c7231f648fe601abc37e81
ETLDevs <etlbdevs@gmail.com>
2023-05-24 12:27:14 +0300

    created styled element for FilterPanel

---
+++ src/lib/components/commons/FilterPanel/FilterPanel.styled.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import { StyledButton } from '../Button/Button.styled';
+
+type IStyledFilterProps = {
+justify?: string;
+width?: string; 
+isButtonGroup?: boolean;
+}
+
+export const StyledFilterPanel = styled.div`
+            display: grid;
+            grid-column: 1/-1;
+            gap: .5rem;
+            position: absolute;
+            top:15%;
+            background-color:#ecf0f6;
+            z-index:1;
+            border-radius: 10px;
+            padding:2%;
+            box-shadow:var(--box-shadow-1);
+`;
+
+export const StyledFilterControlDiv= styled.div<IStyledFilterProps>`
+display:flex;
+flex-wrap: wrap;
+gap:2%;
+justify-content: ${({justify}) => justify ?? 'space-evenly'};
+margin:2% 0
+`;
+
+export const StyledFilterButton = styled(StyledButton)<IStyledFilterProps>`
+width: ${({width}) => width ?? '50%'};
+
+height: 35px;
+border: ${({isButtonGroup}) => isButtonGroup ? '1px solid var(--color-active-light)' : 'none'};
+&:hover {
+box-shadow: none;
+transform: none;
+
+          }
+`;
