81f84af8b620a20b7d9c45c6edd2fe9da24d51be
shlomo glikman <shlomog@capitolis.com>
2023-06-01 09:19:33 +0300

    create COMMON_FIELDS_DISPLAY_TEXTS

--- src/lib/consts/commonFields.ts
+++ src/lib/consts/commonFields.ts
@@ -1,3 +1,5 @@
+import { Lang } from './displayTexts';
+
 export enum ICommonFields {
   Id = 'id',
   CreatedAt = 'createdAt',
@@ -5,3 +7,22 @@ export enum ICommonFields {
   UpdatedAt = 'updatedAt',
   UpdatedBy = 'updatedBy',
 }
+
+type IDisplayTextMapping = Record<ICommonFields, string>;
+
+export const COMMON_FIELDS_DISPLAY_TEXTS: Record<Lang, IDisplayTextMapping> = {
+  he: {
+    [ICommonFields.Id]: 'מזהה',
+    [ICommonFields.CreatedAt]: 'נוצר ב',
+    [ICommonFields.CreatedBy]: 'נוצר עי',
+    [ICommonFields.UpdatedAt]: 'עודכן ב',
+    [ICommonFields.UpdatedBy]: 'עודכן עי',
+  },
+  en: {
+    [ICommonFields.Id]: 'ID',
+    [ICommonFields.CreatedAt]: 'Created At',
+    [ICommonFields.CreatedBy]: 'Created By',
+    [ICommonFields.UpdatedAt]: 'Updated At',
+    [ICommonFields.UpdatedBy]: 'Updated By',
+  },
+};
