52d82709ce69f7b3193aca3a4969813ab20e40ab
ETLDevs <etlbdevs@gmail.com>
2023-06-02 00:51:49 +0300

    created new types for filters prop

--- src/lib/components/commons/FilterPanel/FilterPanel.types.ts
+++ src/lib/components/commons/FilterPanel/FilterPanel.types.ts
@@ -1,4 +1,16 @@
-export type IFilterPanelProps = {};
+export enum IFilterItemType {
+  Date = 'date',
+  Buttons = 'buttons',
+}
+
+export type IFilterItem<T extends string = string> = {
+  type: IFilterItemType;
+  field: T;
+};
+
+export type IFilterPanelProps = {
+  filters: IFilterItem[];
+};
 export type IFilterPanelButtonProps = {};
 
 export type IStatusState = 'active' | 'not-active' | 'cancel';
