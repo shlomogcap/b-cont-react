91ae70dd3456f76e86bafdd7d11f6dd2802c4422
shlomo glikman <shlomog@capitolis.com>
2023-06-08 17:28:27 +0300

    Update NumberInput to use numericFormat

--- src/lib/components/commons/Input/inputs/NumberInput.tsx
+++ src/lib/components/commons/Input/inputs/NumberInput.tsx
@@ -1,12 +1,41 @@
+import { useFormContext } from 'react-hook-form';
 import { InputControl } from '../Input';
 import { StyledInputField } from '../Input.styled';
 import { IInputProps } from '../Input.types';
+import { NumericFormat, NumericFormatProps } from 'react-number-format';
 
-export const NumberInput = (props: Omit<IInputProps, 'inputElement'>) => (
-  <InputControl
-    {...props}
-    inputElement={
-      <StyledInputField type='number' style={{ direction: 'ltr' }} />
-    }
-  />
-);
+type INumberInputProps = Omit<IInputProps, 'inputElement'> & {
+  numericFormatProps?: NumericFormatProps;
+};
+
+export const NumberInput = ({
+  numericFormatProps = {},
+  ...inputControlProps
+}: INumberInputProps) => {
+  const { setValue } = useFormContext();
+  return (
+    <div>
+      <InputControl
+        {...inputControlProps}
+        inputElement={<input type='number' hidden />}
+      />
+      <NumericFormat
+        thousandSeparator
+        allowNegative={false}
+        {...numericFormatProps}
+        onChange={(e) =>
+          setValue(
+            inputControlProps.name,
+            Number(e.target.value.replace(/,/g, '')),
+            {
+              shouldValidate: true,
+            },
+          )
+        }
+        customInput={(inputProps) => (
+          <StyledInputField style={{ direction: 'ltr' }} {...inputProps} />
+        )}
+      />
+    </div>
+  );
+};
