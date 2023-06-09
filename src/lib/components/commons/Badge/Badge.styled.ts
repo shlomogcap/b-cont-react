e841819d485fa74020196e909ca0a908a0fe2726
ETLDevs <etlbdevs@gmail.com>
2023-06-09 08:48:09 +0300

    styled Badge

--- src/lib/components/commons/Badge/Badge.styled.ts
+++ src/lib/components/commons/Badge/Badge.styled.ts
@@ -1,3 +1,11 @@
 import styled from 'styled-components';
 
-export const StyledBadge = styled.div``;
+export const StyledBadge = styled.span`
+  width: 0.7rem;
+  height: 0.7rem;
+  border-radius: 12px;
+  background-color: var(--color-active);
+  position: absolute;
+  top: 1.2rem;
+  right: 1.2rem;
+`;
--- src/lib/components/commons/Badge/Badge.tsx
+++ src/lib/components/commons/Badge/Badge.tsx
@@ -1,5 +1,14 @@
+import { PropsWithChildren } from 'react';
+import { StyledBadge } from './Badge.styled';
 import { IBadgeProps } from './Badge.types';
 
-export const Badge = (props: IBadgeProps) => {
-  return <></>;
+export const Badge = ({
+  children,
+  ...props
+}: PropsWithChildren<IBadgeProps>) => {
+  return (
+    <>
+      <StyledBadge {...props}>{children}</StyledBadge>
+    </>
+  );
 };
