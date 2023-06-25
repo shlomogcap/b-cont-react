ca7310f8875a22e9ec4bf5b5066119a3ff01c0ca
shlomo glikman <shlomog@capitolis.com>
2023-06-25 22:49:48 +0300

    fix uuid() validation

--- src/lib/consts/validation/commonFieldSchema.ts
+++ src/lib/consts/validation/commonFieldSchema.ts
@@ -6,7 +6,7 @@ import {
 } from './validationSchema';
 
 export const COMMON_FIELDS_SCHEMA = z.object({
-  [ECommonFields.Id]: z.union([z.string().uuid(), z.coerce.number()]),
+  [ECommonFields.Id]: z.union([z.string(), z.coerce.number()]),
   [ECommonFields.CreatedAt]: OPTIONAL_DATE_SCHEMA,
   [ECommonFields.CreatedBy]: OPTIONAL_STRING_SCHEMA,
   [ECommonFields.UpdatedAt]: OPTIONAL_DATE_SCHEMA,
