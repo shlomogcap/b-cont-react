e612aac77c7104a66c3364ec522d4ea57ca71d63
shlomo glikman <shlomog@capitolis.com>
2023-06-28 00:16:54 +0300

    start prepare previewMode

--- src/lib/components/ContractSectionForm/MilestonesTable.tsx
+++ src/lib/components/ContractSectionForm/MilestonesTable.tsx
@@ -12,33 +12,152 @@ import {
   StyledValue,
 } from './ContractSectionForm.styled';
 import { IMilestonesTableProps } from './ContractSectionForm.types';
-import { ESectionCalculationType, ESectionFields } from '@/lib/consts/sections';
+import {
+  ESectionCalculationMethod,
+  ESectionCalculationType,
+  ESectionFields,
+} from '@/lib/consts/sections';
 import { useFormContext, useWatch } from 'react-hook-form';
 import { DISPLAY_TEXTS, ETableStates } from '@/lib/consts/displayTexts';
 import { EmptyState } from '../commons/EmptyState';
 import { NumberInput, TextInput } from '../commons/Input';
 import { toNumber } from '@/lib/utils/numberUtils';
+import { generateNumberArray } from '@/lib/utils/arrayUtils';
+import { useCallback } from 'react';
 
 const calcTotalMilestones = (
   ms: IMilestoneDoc[],
   milestoneValueField: EMilestoneFields,
 ): number => ms.reduce((acc, ms) => acc + toNumber(ms[milestoneValueField]), 0);
 
+const useVirtualUnitsArray = ({
+  itemsCount,
+  isPauschal,
+  itemsStartIndex,
+}: {
+  isPauschal: boolean;
+  itemsStartIndex: number;
+  itemsCount: number;
+}) => {
+  const calcArray = useCallback(() => {
+    const startIndex = itemsStartIndex ?? 1;
+    const units = itemsCount || 1;
+    const totalUnits = units + startIndex;
+    return isPauschal ? generateNumberArray(startIndex, totalUnits - 1) : [1];
+  }, [isPauschal, itemsStartIndex, itemsCount]);
+  return calcArray();
+};
+
+export const calcTotalUnitMilestonePlan = ({
+  weight,
+  price,
+  sectionItemsCount,
+  sectionTotalSum,
+  sectionItemPrice,
+  isPauschal,
+  isPercentageSection,
+}: {
+  weight: number;
+  price: number;
+  sectionItemsCount: number;
+  sectionTotalSum: number;
+  sectionItemPrice: number;
+  isPauschal: boolean;
+  isPercentageSection: boolean;
+}) => {
+  if (isPauschal) {
+    return (sectionTotalSum * weight) / 100 / sectionItemsCount;
+  }
+  return isPercentageSection ? sectionItemPrice * (weight / 100) : price;
+};
+
+const MilestoneUnitRow = ({
+  isPreviewMode,
+  itemsCount,
+  milestones,
+  milestoneValueField,
+  isPercentageSection,
+  sectionTotalSum,
+  sectionItemPrice,
+  isPauschal,
+  milestonesTotal,
+  unit,
+}: {
+  itemsCount: number;
+  milestones: IMilestoneDoc[];
+  milestoneValueField: EMilestoneFields.Weight | EMilestoneFields.Price;
+  isPercentageSection: boolean;
+  milestonesTotal: number;
+  sectionTotalSum: number;
+  sectionItemPrice: number;
+  isPauschal: boolean;
+  isPreviewMode: boolean;
+  unit: number | string;
+}) => (
+  <>
+    <StyledIndex>
+      {isPreviewMode ? unit : itemsCount > 1 ? `X ${itemsCount}` : '-'}
+    </StyledIndex>
+    {milestones.map((ms, i) => (
+      <StyledValue key={`body/${ms.id}`}>
+        {isPreviewMode ? (
+          `${calcTotalUnitMilestonePlan({
+            weight: ms[EMilestoneFields.Weight],
+            price: ms[EMilestoneFields.Price],
+            sectionItemsCount: itemsCount,
+            sectionTotalSum,
+            sectionItemPrice,
+            isPauschal,
+            isPercentageSection,
+          }).toLocaleString()}${isPercentageSection ? ' %' : ''}`
+        ) : (
+          <NumberInput
+            name={`milestones[${i}].${milestoneValueField}`}
+            label={MILESTONES_DISPALY_TEXTS.he.fields[milestoneValueField]}
+            numericFormatProps={{ suffix: isPercentageSection ? ' %' : '' }}
+            hideLabel
+          />
+        )}
+      </StyledValue>
+    ))}
+    <StyledTotal>{`${milestonesTotal.toLocaleString()}${
+      isPercentageSection ? ' %' : ''
+    }`}</StyledTotal>
+  </>
+);
+
 export const MilestonesTable = ({
   isLoading,
   isPreviewMode,
 }: IMilestonesTableProps) => {
   const { watch } = useFormContext();
-  const [calculationType, price, itemsCount, totalSum] = useWatch({
+  const [
+    calculationType,
+    calculationMethod,
+    price,
+    itemsCount,
+    itemsStartIndex,
+    totalSum,
+  ] = useWatch({
     name: [
       ESectionFields.CalculationType,
+      ESectionFields.CalculationMethod,
       ESectionFields.ItemPrice,
       ESectionFields.ItemsCount,
+      ESectionFields.ItemsStartIndex,
       ESectionFields.TotalSum,
     ],
   });
+  const isPauschal = calculationMethod === ESectionCalculationMethod.Pauschal;
   const isPercentageSection =
     calculationType === ESectionCalculationType.Percentage;
+
+  const virtualUnits = useVirtualUnitsArray({
+    isPauschal,
+    itemsCount: toNumber(itemsCount),
+    itemsStartIndex: toNumber(itemsStartIndex),
+  });
+
   const milestoneValueField = isPercentageSection
     ? EMilestoneFields.Weight
     : EMilestoneFields.Price;
@@ -75,20 +194,21 @@ export const MilestonesTable = ({
       <StyledTotal>{MILESTONES_DISPALY_TEXTS.he.total}</StyledTotal>
 
       {/* ROWS */}
-      <StyledIndex>{itemsCount > 1 ? `X ${itemsCount}` : '-'}</StyledIndex>
-      {watchMilestones.map((ms, i) => (
-        <StyledValue key={`body/${ms.id}`}>
-          <NumberInput
-            name={`milestones[${i}].${milestoneValueField}`}
-            label={MILESTONES_DISPALY_TEXTS.he.fields[milestoneValueField]}
-            numericFormatProps={{ suffix: isPercentageSection ? ' %' : '' }}
-            hideLabel
-          />
-        </StyledValue>
+      {[...(isPreviewMode ? virtualUnits : ['*'])].map((unit) => (
+        <MilestoneUnitRow
+          key={unit}
+          unit={unit}
+          itemsCount={itemsCount}
+          milestonesTotal={milestonesTotal}
+          isPercentageSection={isPercentageSection}
+          milestoneValueField={milestoneValueField}
+          milestones={watchMilestones}
+          isPreviewMode={isPreviewMode}
+          isPauschal={isPauschal}
+          sectionItemPrice={price}
+          sectionTotalSum={totalSum}
+        />
       ))}
-      <StyledTotal>{`${milestonesTotal.toLocaleString()}${
-        isPercentageSection ? ' %' : ''
-      }`}</StyledTotal>
 
       {/* TOTALS */}
       <StyledGrandTotal>{MILESTONES_DISPALY_TEXTS.he.total}</StyledGrandTotal>
