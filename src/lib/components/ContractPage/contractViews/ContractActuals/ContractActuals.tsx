e3c5dd04203c7be033aff965908f4531f1f93696
shlomo glikman <shlomog@capitolis.com>
2023-07-06 15:50:19 +0300

    prepare confirms block

--- src/lib/components/ContractPage/contractViews/ContractActuals/ContractActuals.tsx
+++ src/lib/components/ContractPage/contractViews/ContractActuals/ContractActuals.tsx
@@ -1,9 +1,15 @@
+import { IAccountDoc } from '@/lib/consts/accounts/AccountDoc';
+import { ContractConfirms } from '../ContractConfirms';
 import { IContractActualsProps } from './ContractActuals.types';
 
 export const ContractActuals = (props: IContractActualsProps) => {
+  const account = {
+    accountStage: 'finish',
+    period: '2020-04',
+  } as IAccountDoc; //TODO: get from context with current account
   return (
     <>
-      <div>status</div>
+      <ContractConfirms acccount={account} />
       <div>chat</div>
       <div>report</div>
     </>
---
+++ src/lib/components/ContractPage/contractViews/ContractConfirms/ContractConfirms.consts.ts
@@ -0,0 +1,26 @@
+export const CONFIRMS_DATA = [
+  {
+    id: 'a',
+    title: 'אתחול',
+    due: 5,
+    confirm: true,
+    confirmAt: '2020-01-01 08:50',
+    confirmBy: 'shlomo',
+  },
+  {
+    id: 'b',
+    title: 'מנהפ',
+    due: 7,
+    confirm: true,
+    confirmAt: '2020-01-02 14:00',
+    confirmBy: 'shlomo',
+  },
+  {
+    id: 'c',
+    title: 'מהנדס חברה',
+    due: 10,
+    confirm: true,
+    confirmAt: '2020-01-03 09:27',
+    confirmBy: 'shlomo',
+  },
+];
