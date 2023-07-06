0211b3d171d21ce38ce9af8817806df64e4387ec
shlomo glikman <shlomog@capitolis.com>
2023-07-06 17:59:07 +0300

    fix icon react/displayName error

--- src/lib/components/icons/SvgIcon/SvgIcon.tsx
+++ src/lib/components/icons/SvgIcon/SvgIcon.tsx
@@ -3,21 +3,21 @@ import { SIZE_MAP } from './SvgIcon.consts';
 import { ISvgIconProps } from './SvgIcon.types';
 import { Ref, forwardRef } from 'react';
 
-export const SvgIcon = forwardRef(
-  (
-    { children, size = 'S', ...rest }: ISvgIconProps,
-    ref: Ref<SVGSVGElement>,
-  ) => {
-    return (
-      <StyledSvgIcon
-        ref={ref}
-        width={SIZE_MAP[size]}
-        height={SIZE_MAP[size]}
-        viewBox='0 0 32 32'
-        {...rest}
-      >
-        {children}
-      </StyledSvgIcon>
-    );
-  },
-);
+const SvgIconContent = (
+  { children, size = 'S', ...rest }: ISvgIconProps,
+  ref?: Ref<SVGSVGElement>,
+) => {
+  return (
+    <StyledSvgIcon
+      ref={ref}
+      width={SIZE_MAP[size]}
+      height={SIZE_MAP[size]}
+      viewBox='0 0 32 32'
+      {...rest}
+    >
+      {children}
+    </StyledSvgIcon>
+  );
+};
+
+export const SvgIcon = forwardRef(SvgIconContent);
