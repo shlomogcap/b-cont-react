45a4cbf4f45e88bbeac52cdc019f8d91150abb8a
shlomo glikman <shlomog@capitolis.com>
2024-04-08 13:32:02 +0300

    refactor getEnumMemberOrSelf to support optional enum object

--- src/lib/utils/enumUtils.ts
+++ src/lib/utils/enumUtils.ts
@@ -1,4 +1,7 @@
 export const getEnumMemberOrSelf = <E extends Record<string, number | string>>(
   key: string,
   enumObject: E,
-) => (Object.keys(enumObject).includes(key) ? enumObject[key as keyof E] : key);
+) =>
+  Object.keys(enumObject ?? {}).includes(key)
+    ? enumObject[key as keyof E]
+    : key;
